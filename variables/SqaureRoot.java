fbc90529d71033e45b22139691953d4879b71adc
Nafarfu <nafarfu@gmail.com>
2016-12-18 09:38:47 +0200

    Exercises
    Training

---
+++ variables/SqaureRoot.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class SqaureRoot {
+
+	public static void main(String[] args) {
+		
+		// Calculate square root numbers from 1 to 5
+		
+		for(int i = 1; i <= 5; i++){
+			
+			System.out.println(i + " * " + i + " is equal to = " + i * i);
+		}
+
+	}
+
+}
